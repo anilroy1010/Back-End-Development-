f9453023a62a53e414b0fbdf11082b2c9fc23bb3
Anil Roy <anilroy.cce@gmail.com>
2024-09-11 00:35:09 +0530

    Read file using native Node

--- 1.2 Native Modules/index.js
+++ 1.2 Native Modules/index.js
@@ -1,5 +1,12 @@
 const fs=require("fs");
-fs.writeFile("message.txt", "Hello From node js by Anil!",(err)=>{
+//fs.writeFile("message.txt", "Hello From node js by Anil!",(err)=>{
+ //   if(err)throw err;
+ //   console.log("The file has been saved!");
+//});
+
+fs.readFile("./message.txt", "utf8",(err,data)=>{
     if(err)throw err;
-    console.log("The file has been saved!");
+    console.log(data);
 });
+
+
